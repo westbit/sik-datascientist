ce276b30d44efe3a45dcfe54e687021970e0c50a
Erik Jensen <eje@westbit.dk>
2024-03-31 14:07:05 +0200

    Tilføjet Kaldenavn og Stedord til ansoegning.py

--- ansoegning.py
+++ ansoegning.py
@@ -1,4 +1,11 @@
 class PyUdvikler:
     def __init__(self):
         self.cases = []
+        self.kaldenavn = "Erik Valbjørn Jensen"
+        self.stedord = Stedord("Erik", "Eriks")
 
+
+class Stedord:
+    def __init__(self, grundform, ejefald):
+        self.grundform = grundform
+        self.ejefald = ejefald
